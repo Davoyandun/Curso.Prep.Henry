e11a31e25c5507447e196287c290882fd2605b14
Davo <yandundavid@gmail.com>
2021-03-11 23:43:48 -0500

    Terminado js5

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -118,8 +118,14 @@ function pasarUsuarioAPremium(usuarios) {
   // Define cada propiedad "esPremium" de cada objeto como "true"
   // Devuelve el array de usuarios
   // Tu código:
-  usuarios.usuario.
+for (let i = 0; i < usuarios.length; i++) {
+  usuarios[i].esPremium = true 
+  
 }
+return usuarios
+
+}
+
 
 function sumarLikesDeUsuario(usuario) {
   // "usuario" tiene una propiedad llamada "posts" que es un array
@@ -128,6 +134,12 @@ function sumarLikesDeUsuario(usuario) {
   // Suma todos los likes de todos los objetos "post"
   // Devuelve la suma
   // Tu código:
+  var suma = 0
+  for (let i = 0; i < usuario.posts.length; i++) {
+     suma += usuario.posts[i]["likes"]
+    
+  }
+return suma
 }
 
 function agregarMetodoCalculoDescuento(producto) {
@@ -140,7 +152,14 @@ function agregarMetodoCalculoDescuento(producto) {
   // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
-}
+   producto.calcularPrecioDescuento = function (){
+        return producto.precio = producto.precio - (producto.precio * producto.porcentajeDeDescuento)
+  
+    }
+     return producto
+  }
+ 
+
 
 // No modificar nada debajo de esta línea
 // --------------------------------
